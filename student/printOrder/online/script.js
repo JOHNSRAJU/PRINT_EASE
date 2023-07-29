34ef7727fba3561f9c22960456820f39d57a710a
Johns Raju <83854933+JOHNSRAJU@users.noreply.github.com>
2023-07-29 21:16:50 +0000

    onlineDeleted

--- student/printOrder/online/script.js
+++
@@ -1,9 +0,0 @@
-let popup=document.getElementById("popup");
-function openpopup(){
-    popup.classList.add("open-popup")
-    //add 5sec delay
-    setTimeout(()=>{
-        window.location.href ="../../invoice/index.html?docid="+docid;
-    },5000)
-
-}
